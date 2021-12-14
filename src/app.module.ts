d8cfe7e98bbdc1a20ac053020428d4b2ffcf945e
Gino Ascencio <gino.francisco@hotmail.com>
2021-12-14 07:58:49 -0500

    FIX: Module updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ImagesModule } from './images/images.module';
+import { ClientsModule } from './clients/clients.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
@@ -16,6 +19,8 @@ import { ImagesModule } from './images/images.module';
       synchronize: true,
       // logging: true,
     }),
+    ClientsModule,
+    CategoriesModule,
   ],
 })
 export class AppModule { }
