10607bffd5500e677861a2f4f6d189a00d8f9249
Gino Ascencio <gino.francisco@hotmail.com>
2022-02-03 21:39:41 -0500

    FIX Auth in all modules

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClientRepository } from 'db/repositories/client.repository';
+import { AuthModule } from 'src/auth/auth.module';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ClientRepository])],
+  imports: [TypeOrmModule.forFeature([ClientRepository]), AuthModule],
   controllers: [ClientsController],
   providers: [ClientsService]
 })
--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -3,11 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from 'db/entities/category.entity';
 import { Client } from 'db/entities/client.entity';
 import { ImageRepository } from 'db/repositories/image.repository';
+import { AuthModule } from 'src/auth/auth.module';
 import { ImagesController } from './images.controller';
 import { ImagesService } from './images.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ImageRepository, Client, Category])],
+  imports: [
+    TypeOrmModule.forFeature([ImageRepository, Client, Category]),
+    AuthModule,
+  ],
   controllers: [ImagesController],
   providers: [ImagesService],
 })
